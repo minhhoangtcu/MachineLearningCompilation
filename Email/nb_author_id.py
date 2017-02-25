f6e07739c7b09e02ca94956dd76fca4b5f48aaf0
Minh Hoang TCU <minh.hoang@tcu.edu>
2017-02-25 14:29:00 -0600

    finish email problem on udacity

--- Email/nb_author_id.py
+++ Email/nb_author_id.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
 """
     This is the code to accompany the Lesson 1 (Naive Bayes) mini-project.
@@ -18,3 +20,15 @@ from email_preprocess import preprocess
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+
+gnb = GaussianNB()
+
+t0 = time()
+clf = gnb.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "testing time:", round(time()-t0, 3), "s"
+
+score = accuracy_score(labels_test, pred)
